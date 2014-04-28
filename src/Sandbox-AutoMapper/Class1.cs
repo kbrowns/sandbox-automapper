a5bc6909a92e9f8d6d7b8279534b195abbd702df
Kelly Brownsberger <kelly.brownsberger@csgi.com>
2014-04-28 17:23:17 -0400

    a little playing

---
+++ src/Sandbox-AutoMapper/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace Sandbox_AutoMapper
+{
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+            AutoMapperConfiguration.Configure();
+        }
+    }
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Map_Bidirectionally_With_ForMember()
+        {
+            var summary = Mapper.Map<Order, OrderSummary>(new Order {OrderNumber = "1234"});
+            Assert.That("1234", Is.EqualTo(summary.OrderNum));
+
+            var order = Mapper.Map<OrderSummary, Order>(summary);
+            Assert.That(summary.OrderNum, Is.EqualTo(order.OrderNumber));
+        }
+    }
+
+    public class Order
+    {
+        public string OrderNumber { get; set; }
+    }
+
+    public class OrderSummary
+    {
+        public string OrderNum { get; set; }
+    }
+
+    public class OrderingMap : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Order, OrderSummary>()
+                .ForMember(dest => dest.OrderNum, opt => opt.MapFrom(src => src.OrderNumber))
+                .ReverseMap()
+                .ForMember(dest => dest.OrderNumber, opt => opt.MapFrom(src => src.OrderNum));
+        }
+    }
+
+    public static class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(x => GetConfiguration(Mapper.Configuration));
+        }
+
+        private static void GetConfiguration(IConfiguration configuration)
+        {
+            var profiles = typeof(OrderingMap).Assembly.GetTypes().Where(x => typeof(Profile).IsAssignableFrom(x));
+            foreach (var profile in profiles)
+            {
+                configuration.AddProfile(Activator.CreateInstance(profile) as Profile);
+            }
+        }
+    }
+}
---
+++ src/Sandbox-AutoMapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sandbox-AutoMapper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sandbox-AutoMapper")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a3295f1-2297-440c-a1bd-2c8d7e90081b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
